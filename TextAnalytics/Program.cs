45ebc061c2af4492f26820a9b19c7620547d9320
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2018-09-14 00:09:43 -0700

    Updating for Ignite.
    This is the combined code from https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/csharp.

--- TextAnalytics/Program.cs
+++ TextAnalytics/Program.cs
@@ -2,31 +2,49 @@ using System;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
 using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
 using System.Collections.Generic;
+using Microsoft.Rest;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace TextAnalyticsSample
+namespace ConsoleApp1
 {
     class Program
     {
+        /// <summary>
+        /// Container for subscription credentials. Make sure to enter your valid key.
+        /// </summary>
+        class ApiKeyServiceClientCredentials : ServiceClientCredentials
+        {
+            public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                request.Headers.Add("Ocp-Apim-Subscription-Key", "ENTER KEY HERE");
+                return base.ProcessHttpRequestAsync(request, cancellationToken);
+            }
+        }
+
         static void Main(string[] args)
         {
+
             // Create a client.
-            ITextAnalyticsAPI client = new TextAnalyticsAPI();
-            client.AzureRegion = AzureRegions.Westcentralus;
-            client.SubscriptionKey = "ENTER YOUR KEY HERE";
+            ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials())
+            {
+                Endpoint = "https://westus.api.cognitive.microsoft.com"
+            };
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // Extracting language
+			// Extracting language.
             Console.WriteLine("===== LANGUAGE EXTRACTION ======");
 
-            LanguageBatchResult result = client.DetectLanguage(
-                    new BatchInput(
-                        new List<Input>()
+            var result =  client.DetectLanguageAsync(new BatchInput(
+                    new List<Input>()
                         {
                           new Input("1", "This is a document written in English."),
                           new Input("2", "Este es un document escrito en Español."),
                           new Input("3", "这是一个用中文写的文件")
-                        }));
+                    })).Result;
 
             // Printing language results.
             foreach (var document in result.Documents)
@@ -34,21 +52,19 @@ namespace TextAnalyticsSample
                 Console.WriteLine("Document ID: {0} , Language: {1}", document.Id, document.DetectedLanguages[0].Name);
             }
 
-            // Getting key-phrases
+			// Getting key phrases.
             Console.WriteLine("\n\n===== KEY-PHRASE EXTRACTION ======");
 
-            KeyPhraseBatchResult result2 = client.KeyPhrases(
-                    new MultiLanguageBatchInput(
+            KeyPhraseBatchResult result2 = client.KeyPhrasesAsync(new MultiLanguageBatchInput(
                         new List<MultiLanguageInput>()
                         {
                           new MultiLanguageInput("ja", "1", "猫は幸せ"),
                           new MultiLanguageInput("de", "2", "Fahrt nach Stuttgart und dann zum Hotel zu Fu."),
                           new MultiLanguageInput("en", "3", "My cat is stiff as a rock."),
                           new MultiLanguageInput("es", "4", "A mi me encanta el fútbol!")
-                        }));
+                        })).Result;
 
-
-            // Printing keyphrases
+            // Printing key phrases.
             foreach (var document in result2.Documents)
             {
                 Console.WriteLine("Document ID: {0} ", document.Id);
@@ -61,10 +77,10 @@ namespace TextAnalyticsSample
                 }
             }
 
-            // Extracting sentiment
+			// Analyzing sentiment.
             Console.WriteLine("\n\n===== SENTIMENT ANALYSIS ======");
 
-            SentimentBatchResult result3 = client.Sentiment(
+            SentimentBatchResult result3 = client.SentimentAsync(
                     new MultiLanguageBatchInput(
                         new List<MultiLanguageInput>()
                         {
@@ -72,17 +88,31 @@ namespace TextAnalyticsSample
                           new MultiLanguageInput("en", "1", "This was a waste of my time. The speaker put me to sleep."),
                           new MultiLanguageInput("es", "2", "No tengo dinero ni nada que dar..."),
                           new MultiLanguageInput("it", "3", "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."),
-                        }));
+                        })).Result;
 
 
-            // Printing sentiment results
+            // Printing sentiment results.
             foreach (var document in result3.Documents)
             {
                 Console.WriteLine("Document ID: {0} , Sentiment Score: {1:0.00}", document.Id, document.Score);
             }
 
-            Console.WriteLine("Press ENTER to exit.");
-            Console.ReadLine();
+			// Linking entities
+            Console.WriteLine("\n\n===== ENTITY LINKING ======");
+
+            EntitiesBatchResult result4 = client.EntitiesAsync(
+                    new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+                            new MultiLanguageInput("en", "0", "I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable."),
+                            new MultiLanguageInput("en", "1", "The Seattle Seahawks won the Super Bowl in 2014."),
+                        })).Result;
+
+            // Printing entity results.
+            foreach (var document in result4.Documents)
+            {
+                Console.WriteLine("Document ID: {0} , Entities: {1}", document.Id, String.Join(", ", document.Entities.Select(entity => entity.Name)));
+            }
         }
     }
 }
